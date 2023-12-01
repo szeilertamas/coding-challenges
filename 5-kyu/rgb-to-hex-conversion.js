d16a00e64a393fcca55306f2418cc25a3aad7c9f
Tamas Szeiler <szeilertamas@gmail.com>
2023-12-01 16:49:24 +0100

    Refactored solution

--- 5-kyu/rgb-to-hex-conversion.js
+++ 5-kyu/rgb-to-hex-conversion.js
@@ -1,11 +1,9 @@
 function rgb(r, g, b) {
-    return [
-      r < 0 ? '00' : r > 255 ? 'FF' : r.toString(16).padStart(2, "0"),
-      g < 0 ? '00' : g > 255 ? 'FF' : g.toString(16).padStart(2, "0"),
-      b < 0 ? '00' : b > 255 ? 'FF' : b.toString(16).padStart(2, "0"),
-    ]
-      .join("")
-      .toUpperCase();
+    function convert(c) {
+      return c < 0 ? "00" : c > 255 ? "FF" : c.toString(16).padStart(2, "0");
+    }
+  
+    return [convert(r), convert(g), convert(b)].join("").toUpperCase();
   }
 
 //   The rgb function is incomplete.
