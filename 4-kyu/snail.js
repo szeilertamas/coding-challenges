cd3f215491d8664457671448b834fb8205371284
Tamas Szeiler <43131215+szeilertamas@users.noreply.github.com>
2023-12-22 16:56:49 +0100

    Add snail function

---
+++ 4-kyu/snail.js
@@ -0,0 +1,38 @@
+snail = function (array) {
+    const result = [];
+  
+    while (array.length > 0) {
+      result.push(...array.shift());
+  
+      for (let i = 0; i < array.length; i++) {
+        result.push(array[i].pop());
+      }
+  
+      if (array.length > 0) {
+        result.push(...array.pop().reverse());
+      }
+  
+      for (let i = array.length - 1; i >= 0; i--) {
+        result.push(array[i].shift());
+      }
+    }
+    return result;
+  };
+  
+// DESCRIPTION:
+// Snail Sort
+// Given an n x n array, return the array elements arranged from
+// outermost elements to the middle element, traveling clockwise.
+
+// array = [[1,2,3],
+//          [4,5,6],
+//          [7,8,9]]
+// snail(array) #=> [1,2,3,6,9,8,7,4,5]
+
+// For better understanding,
+// please follow the numbers of the next array consecutively:
+
+// array = [[1,2,3],
+//          [8,9,4],
+//          [7,6,5]]
+// snail(array) #=> [1,2,3,4,5,6,7,8,9]
